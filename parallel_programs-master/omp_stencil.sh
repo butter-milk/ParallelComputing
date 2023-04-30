645679097b0ad0bfeccc5a8b9037825d7a5dd662
LoadingWave <tda.posthuma@gmail.com>
2023-04-30 11:31:45 +0200

    Point to the correct target for compilation

--- parallel_programs-master/omp_stencil.sh
+++ parallel_programs-master/omp_stencil.sh
@@ -7,8 +7,8 @@
 #SBATCH --output=stencil_omp.out
 
 # Compile on the machine, not the head node
-make bin/2dstencil_omp
+make bin/stencil_omp
 
 for P in 1 2 3 4 5 6; do
-    OMP_NUM_THREADS="$P" bin/2dstencil_omp 10000 100 > results/stencil_omp_"$P".txt
+    OMP_NUM_THREADS="$P" bin/stencil_omp 10000 100 > results/stencil_omp_"$P".txt
 done
