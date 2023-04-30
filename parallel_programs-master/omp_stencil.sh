e6b94efdc7214fb504018f850441facdd4666195
LoadingWave <tda.posthuma@gmail.com>
2023-04-30 12:54:31 +0200

    Make n and i actually variables

--- parallel_programs-master/omp_stencil.sh
+++ parallel_programs-master/omp_stencil.sh
@@ -9,8 +9,8 @@
 # Compile on the machine, not the head node
 make bin/stencil_omp
 
-n = 5000000
-i = 1000
+n=5000000
+i=1000
 
 for P in 1 2 4 8 16 32; do
     OMP_NUM_THREADS="$P" bin/stencil_omp ${n} ${i} > results/stencil_omp_"$n"_"$i"_"$P".txt
