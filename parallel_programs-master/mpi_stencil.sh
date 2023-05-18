1c595b3d0a370aba09a5924e206b6863ca569778
LoadingWave <tda.posthuma@gmail.com>
2023-05-18 15:42:32 +0200

    Fix NODES incrementing issue

--- parallel_programs-master/mpi_stencil.sh
+++ parallel_programs-master/mpi_stencil.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-NODES=1
-
 if [ $# -lt 2 ]
     then
         echo "Supply 'n' and 'iterations' please"
         exit 1
 fi
 
+NODES=1
 
 while [ $NODES -le 8 ]
 do
@@ -22,8 +21,8 @@ do
         sed -i "s/ITER/$2/g" mpi_stencil_"$NTASKS"_"$EXPRM".sh
         sbatch mpi_stencil_"$NTASKS"_"$EXPRM".sh
 
-        rm mpi_stencil_"$NTASKS"_"$EXPRM".sh
-
-        NODES=$(( 2 * NODES ))
+        rm mpi_stencil_"$NTASKS"_"$EXPRM".sh    
     done
+
+    NODES=$(( 2 * NODES ))
 done
