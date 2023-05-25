d6e382066603788bd6653d12075556457434b0f6
LoadingWave <tda.posthuma@gmail.com>
2023-05-25 16:24:57 +0200

    Add sperate files for debugging and measurements

---
+++ parallel_programs-master/mpi_stencil_rep.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]
+    then
+        echo "Supply 'n' and 'iterations' please"
+        exit 1
+fi
+
+NODES=1
+
+while [ $NODES -le 8 ]
+do
+
+    NTASKS=$(( 8 * NODES ))
+    
+    sed "s/NODES/$NODES/g" mpi_stencil_rep.sh.template > mpi_stencil_"$NTASKS".sh
+    sed -i "s/NTASKS/$NTASKS/g" mpi_stencil_"$NTASKS".sh
+    sed -i "s/LEN/$1/g" mpi_stencil_"$NTASKS".sh
+    sed -i "s/ITER/$2/g" mpi_stencil_"$NTASKS".sh
+    sbatch mpi_stencil_"$NTASKS".sh
+
+    rm mpi_stencil_"$NTASKS".sh    
+
+    NODES=$(( 2 * NODES ))
+done
