f042a8ebbf98cab65ad5b8f15f729c683244c9a0
LoadingWave <tda.posthuma@gmail.com>
2023-05-25 16:05:02 +0200

    Add check for number of ranks devides n

--- parallel_programs-master/src/mpi/stencil_opt.c
+++ parallel_programs-master/src/mpi/stencil_opt.c
@@ -232,48 +232,53 @@ int main(int argc, char **argv)
     assuming that p divides n, so if we use 1,2,4,8,16,32 threads, n should always divide 32!
     furthermore, n should divide SPACEBLOCK as well, meaning that n should be a multiple of lcm(32,1000) = 4000
     */
-    int my_rank, p;
+    int my_rank, rank_count;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    MPI_Comm_size(MPI_COMM_WORLD, &rank_count);
+
+    if (n % rank_count != 0) {
+        printf("I am lazy, so assumed that rank count divides n.");
+        return EXIT_FAILURE;
+    }
 
     // Add Size checks
     int size;
     REAL *in, *out;
-    if (my_rank == 0 || my_rank == p - 1){
+    if (my_rank == 0 || my_rank == rank_count - 1){
         // If were dealing with either of the ends of the array, we only need to deal overlap on one side
-        size = n/p + TIMEBLOCK; 
+        size = n/rank_count + TIMEBLOCK; 
         in = calloc(size,  sizeof(REAL));
         out = malloc(size * sizeof(REAL));
         if (my_rank == 0) {
             in[0] = 100;
         }
-        if (my_rank == p - 1) {
+        if (my_rank == rank_count - 1) {
             in[n + TIMEBLOCK - 1]=1000;
         }
-        if ((my_rank * (n/p) - TIMEBLOCK) <= n/2 && ((my_rank + 1) * (n/p) + TIMEBLOCK) >= n/2) {
+        if ((my_rank * (n/rank_count) - TIMEBLOCK) <= n/2 && ((my_rank + 1) * (n/rank_count) + TIMEBLOCK) >= n/2) {
             if (my_rank == 0) {
                 // if myrank = 0, the starting index is my_rank * (n/p) = 0 * n/p = 0
                 in[n/2] = n;
             } else {
                 // In the outer if we have just checked that this index is non-negative, so this is valid
-                in[n/2 - (my_rank * n/p - TIMEBLOCK)] = n;
+                in[n/2 - (my_rank * n/rank_count - TIMEBLOCK)] = n;
             }
         }
     } else {
         // In a middle block, we have overlap on two sides, so we allocate 2 * timeblock as buffer
-        size = n/p + 2*TIMEBLOCK ;
-        in = calloc(n/p + 2*TIMEBLOCK, sizeof(REAL));
-        out = malloc((n/p +2*TIMEBLOCK) * sizeof(REAL));
+        size = n/rank_count + 2*TIMEBLOCK ;
+        in = calloc(n/rank_count + 2*TIMEBLOCK, sizeof(REAL));
+        out = malloc((n/rank_count +2*TIMEBLOCK) * sizeof(REAL));
 
         // The first and the last element don't need to be set, because that is in either the first or the last threads
         // Check if this thread contains the middle element
-        if ((my_rank * (n/p) - TIMEBLOCK) <= n/2 && ((my_rank + 1) * (n/p) + TIMEBLOCK) >= n/2) {
-            in[n/2 - (my_rank * n/p - TIMEBLOCK)] = n;
+        if ((my_rank * (n/rank_count) - TIMEBLOCK) <= n/2 && ((my_rank + 1) * (n/rank_count) + TIMEBLOCK) >= n/2) {
+            in[n/2 - (my_rank * n/rank_count - TIMEBLOCK)] = n;
         }
     }
     
     double duration;
-    TIME(duration, Stencil(&in, &out, size, iterations, my_rank, p););
+    TIME(duration, Stencil(&in, &out, size, iterations, my_rank, rank_count););
     if(my_rank==0){printf("%lf %lf\n", duration, iterations * (n-2) * 5 / 1000000000 /duration);}
     MPI_Finalize();
     
