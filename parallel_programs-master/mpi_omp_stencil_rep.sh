46df2b73423deb5980043b7f0d310347d7c9c951
butter-milk <kevinvandeglind@gmail.com>
2023-05-29 15:58:21 +0200

    this branch is now deprecated :)

--- parallel_programs-master/mpi_omp_stencil_rep.sh
+++ parallel_programs-master/mpi_omp_stencil_rep.sh
@@ -6,21 +6,21 @@ if [ $# -lt 2 ]
         exit 1
 fi
 
-NODES=1
+NODES=8
 OMPTHREADS=4
 while [ $NODES -le 8 ]
 do
 
     NTASKS=$(( 8 * NODES ))
     
-    sed "s/NODES/$NODES/g" mpi_stencil_rep.sh.template > mpi_omp_stencil_"$NTASKS".sh
+    sed "s/NODES/$NODES/g" mpi_omp_stencil_rep.sh.template > mpi_omp_stencil_"$NTASKS".sh
     sed -i "s/NTASKS/$NTASKS/g" mpi_omp_stencil_"$NTASKS".sh
     sed -i "s/LEN/$1/g" mpi_omp_stencil_"$NTASKS".sh
     sed -i "s/ITER/$2/g" mpi_omp_stencil_"$NTASKS".sh
     sed -i "s/OMPTHREADS/$OMPTHREADS/g" mpi_omp_stencil_"$NTASKS".sh
     sbatch mpi_omp_stencil_"$NTASKS".sh
 
-    rm mpi_stencil_"$NTASKS".sh    
+    rm mpi_omp_stencil_"$NTASKS".sh    
 
     NODES=$(( 2 * NODES ))
 done
